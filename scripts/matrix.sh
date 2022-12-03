1d9552c0470d87e82cb95b18afc01ad41d09c432
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-03 20:18:49 +0000

    fix: dictionary matrix

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
-nix develop -c -- bash -c "cue export $REPO_ROOT/matrix.cue | jq -c -r '[(.versions|keys),(.skus|keys)] | combinations | { ( ( \"packer-\" + .[0] + \"-\" + .[1] ) | split(\".\") | join(\"-\") ): {\"kube_version\": .[0], \"sku\": .[1]}}' | shuf | head -n 5"
+nix develop -c -- bash -c "cue export $REPO_ROOT/matrix.cue | jq -c -r '[(.versions|keys),(.skus|keys)] | combinations | { ( ( \"packer-\" + .[0] + \"-\" + .[1] ) | split(\".\") | join(\"-\") ): {\"kube_version\": .[0], \"sku\": .[1]}}' | jq -s add"
