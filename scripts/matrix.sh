e40ad3774094488d0284d63606382e4e941e0965
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-03 18:06:16 +0000

    fix: azure cli and sub id

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
-cue export "$REPO_ROOT/matrix.cue" | jq -c -r '[(.versions|keys),(.skus|keys)] | combinations | { ( .[0] + "-" + .[1] ): {"kube_version": .[0], "sku": .[1]}}'
+cue export "$REPO_ROOT/matrix.cue" | jq -c -r '[(.versions|keys),(.skus|keys)] | combinations | { ( "packer" + .[0] + "-" + .[1] ): {"kube_version": .[0], "sku": .[1]}}'
