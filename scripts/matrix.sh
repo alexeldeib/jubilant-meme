5ce9c24bd5961a92f28a0f09d82902842c7cca14
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-04 03:09:29 +0000

    fix: matrix config for hyperv gen

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/.
 
-nix develop -c cue export $REPO_ROOT/matrix.cue > matrix.json
-nix develop -c -- bash -c "cat matrix.json| jq -c -r '[(.versions|keys),(.skus|keys)] | combinations | { ( ( \"packer-\" + .[0] + \"-\" + .[1] ) | split(\".\") | join(\"-\") ): {\"kube_version\": .[0], \"sku\": .[1]}}' | jq -n 'reduce inputs as \$in (null; . + \$in)'" | sponge matrix.json
+nix develop -c -- bash -c "cue export $REPO_ROOT/matrix.cue | jq -c -r '[(.versions|keys),(.skus|to_entries)] | combinations | { ( ( \"packer-\" + .[0] + \"-\" + .[1].key ) | split(\".\") | join(\"-\") ): {\"kube_version\": .[0], \"sku\": .[1].key, \"config\": .[1].value }}' | jq -n 'reduce inputs as \$in (null; . + \$in)'"
