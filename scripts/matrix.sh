0cc2b53773bfebd17949a86e8cb25649316dc630
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-04 03:25:50 +0000

    fix: matrix path

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/.
+REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
 nix develop -c -- bash -c "cue export $REPO_ROOT/matrix.cue | jq -c -r '[(.versions|keys),(.skus|to_entries)] | combinations | { ( ( \"packer-\" + .[0] + \"-\" + .[1].key ) | split(\".\") | join(\"-\") ): {\"kube_version\": .[0], \"sku\": .[1].key, \"config\": .[1].value }}' | jq -n 'reduce inputs as \$in (null; . + \$in)'"
