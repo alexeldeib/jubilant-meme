e92eea618162db1e51e16039cc44b32820805781
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-18 02:10:22 +0000

    debug: timing

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-curl -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+res=$(curl -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
 echo "started at $(date -Isec)"
-until test "$res" == "0"; do
+until test "$res" == "200"; do
    echo "the curl command failed with: $res"
-   curl -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
-   res=$?
+res=$(curl -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")   res=$?
 done
 curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
 msg="imds reachable at $(date -Isec)"
