9439de8cea176728b04d2403539c78ff42695a45
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-18 01:12:00 +0000

    fix: shebang

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
 res=$?
 echo "started at $(date -Isec)"
