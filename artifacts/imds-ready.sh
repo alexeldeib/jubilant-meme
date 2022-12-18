7bb53b3208573b8858c1b976e9fa625c1acd43a1
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-18 01:58:28 +0000

    debug: exit code

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
+curl -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
 echo "started at $(date -Isec)"
 until test "$res" == "0"; do
    echo "the curl command failed with: $res"
-   curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+   curl -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
    res=$?
 done
+curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
 msg="imds reachable at $(date -Isec)"
 # systemd-notify --ready --status="$msg"
