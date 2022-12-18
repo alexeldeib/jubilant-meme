bdf8df8935488db2a9f84de52f80096bee42e436
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-18 01:46:25 +0000

    fix: logic

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
-res=$?
 echo "started at $(date -Isec)"
-until test "$res" != "0"; do
+until test "$res" == "0"; do
    echo "the curl command failed with: $res"
+   curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+   res=$?
 done
 msg="imds reachable at $(date -Isec)"
 # systemd-notify --ready --status="$msg"
