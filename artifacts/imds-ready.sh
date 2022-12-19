dc62bab247c4d6bb5292c6e40cd21836da3182a2
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-19 19:38:01 +0000

    debug: move imds-ready after network-pre

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -6,6 +6,6 @@ until test "$res" == "200"; do
    res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")   res=$?
    sleep 1
 done
-curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
+# curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
 msg="imds reachable at $(date -Isec)"
 # systemd-notify --ready --status="$msg"
