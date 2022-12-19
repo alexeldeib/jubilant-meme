ad9f8100ee25feaf4e1ae87cdf8a993f804f6909
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-19 19:14:22 +0000

    debug: imds-ready throttling

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-res=$(curl -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
+res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
 echo "started at $(date -Isec)"
 until test "$res" == "200"; do
    echo "the curl command failed with: $res"
-res=$(curl -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")   res=$?
+   res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")   res=$?
+   sleep 1
 done
 curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
 msg="imds reachable at $(date -Isec)"
