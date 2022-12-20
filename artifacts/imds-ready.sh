23fb223724f1ce7dbee8e2c9dc6bb8a1c1fc23de
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-20 23:28:22 +0000

    debug: tty and notail

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
-echo "started at $(date -Isec)"
+start="$(date -Isec)"
+echo "started at $start"
 until test "$res" == "200"; do
    echo "the curl command failed with: $res"
    res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
    sleep 1
 done
+echo "started at $start"
 # curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
 msg="imds reachable at $(date -Isec)"
 # systemd-notify --ready --status="$msg"
