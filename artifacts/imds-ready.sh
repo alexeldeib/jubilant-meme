55ff76ca60147f5d0c55c83410ac925494d9b2b2
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-20 23:03:37 +0000

    fix: stray ret=$?

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -3,7 +3,7 @@ res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "
 echo "started at $(date -Isec)"
 until test "$res" == "200"; do
    echo "the curl command failed with: $res"
-   res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")   res=$?
+   res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
    sleep 1
 done
 # curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
