9be926310fdda3f3bb40886c744fec2ae35af86e
Alexander Eldeib <alexeldeib@gmail.com>
2022-12-21 19:04:18 +0000

    fix: actually log the readiness message outside dbus

--- artifacts/imds-ready.sh
+++ artifacts/imds-ready.sh
@@ -11,4 +11,5 @@ done
 echo "started at $start"
 # curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq
 msg="imds reachable at $(date -Isec)"
+echo "$msg"
 systemd-notify --ready --status="$msg"
